70cbfc6b6da8af87c8c9dcbf79e920839dd86ffc
NextProduct.io <info@nextproduct.io>
2026-03-15 01:20:01 +0100

    Configure custom domain deployment for nextproduct.io

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'nextproduct-io';
-const basePath = isGitHubActions ? `/${repoName}` : '';
-
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined,
   images: {
     unoptimized: true,
   },
